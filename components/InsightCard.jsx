aa54446d5255d855f25504ea9bf7e553e3ccd495
unknown <animfahad32.com>
2023-03-25 01:45:42 +0600

    feedback section added

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 import { fadeIn } from "../utils/motion";
 const InsightCard = ({ imgUrl, title, subtitle, index }) => (
   <motion.div
-    variant={fadeIn("up", "spring", index * 0.5, 1)}
+    variants={fadeIn("up", "spring", index * 0.5, 1)}
     className="flex md:flex-row flex-col gap-4"
   >
     <img
--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -1,8 +1,74 @@
-'use client';
+"use client";
+
+import { motion } from "framer-motion";
+
+import styles from "../styles";
+import { staggerContainer, fadeIn, zoomIn } from "../utils/motion";
+
+import { TypingText, TitleText } from "../components";
 
 const Feedback = () => (
-  <section>
-    Feedback section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: "false", amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col mb-10`}
+    >
+      <TypingText title="| Feedback" textStyles="text-center" />
+      <TitleText
+        title={<>Testimonials from Our Valued Owne</>}
+        textStyles="text-center"
+      />
+    </motion.div>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: "false", amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-6`}
+    >
+      <motion.div
+        variants={fadeIn("right", "tween", 0.2, 1)}
+        className="flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[#6a6a6a] relative"
+      >
+        <div className="feedback-gradient" />
+        <div>
+          <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40px] leading-[36px] text-white">
+            Xeno
+          </h4>
+          <p className="mt-8 font-normal sm:text-[18px] text-[12px] sm:leading-[22px] leading-[16px] text-white">
+            Founder | DigitalDandies
+          </p>
+        </div>
+        <p className="mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[44px] leading-[39px] text-white">
+          Welcome to DigitalDandies! As an owner of this platform, I invite you
+          to explore the endless possibilities of the metaverse. Join us on this
+          exciting journey and discover a new world of digital exploration!
+        </p>
+      </motion.div>
+      <motion.div
+        variants={fadeIn("left", "tween", 0.2, 1)}
+        className="relative flex-1 flex justify-center items-center"
+      >
+        <img
+          src="planet-09.png"
+          alt="plant-09"
+          className="w-full lg:h-[610px] h-auto min-h-[210px] object-cover rounded-[40px]"
+        />
+        <motion.div
+          variants={zoomIn(0.4, 1)}
+          className="lg:block hidden absolute -left-[10%] top-[3%]"
+        >
+          <img
+            src="/explore.png"
+            alt="stamp"
+            className="w-[155px] h-[155px] object-contain"
+          />
+        </motion.div>
+      </motion.div>
+    </motion.div>
   </section>
 );
 
